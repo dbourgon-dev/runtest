e05e8ad8bcb9e04c4e32716e91580a2b1040310c
David Bourgon <dbourgon.dev@gmail.com>
2021-05-17 05:02:15 +0200

    Added TypeItem subclasses, move type logic validation to them, change Test to use the new Item subclasses

--- src/Item.php
+++ src/Item.php
@@ -11,10 +11,6 @@ class Item {
     public $sell_in;
     public $quality;
 
-    const AGED_BRIE = 'Aged Brie';
-    const BACKSTAGE_PASS = 'Backstage passes to a TAFKAL80ETC concert';
-    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
-
     function __construct($name, $sell_in, $quality) {
         $this->name = $name;
         $this->sell_in = $sell_in;
@@ -24,56 +20,25 @@ class Item {
     public function getName()
     {
         return $this->name;
-    }
+    }   
 
+    public function updateItem()
+    {
+        $this->decreaseQualityBy(1);
+        $this->decreaseSellInBy(1);  
 
-    public function updateItemByType() 
-    {        
-        switch($this->getName())
-        {
-            case Item::AGED_BRIE:
-                $this->increaseQualityBy(1);  
-                $this->decreaseSellInBy(1);  
-                
-                if ($this->hasPassedOut()) {
-                    $this->increaseQualityBy(1);
-                }
-                break;
-            
-            case Item::BACKSTAGE_PASS:
-                $this->increaseQualityForBackstagePass();                      
-                $this->decreaseSellInBy(1);  
-                
-                if($this->hasPassedOut()){
-                    $this->loseAllQuality(); 
-                } 
-                break;
-            
-            case Item::SULFURAS:
-                break;
-
-            default:
-                $this->decreaseQualityBy(1);
-                $this->decreaseSellInBy(1);  
-
-                if($this->hasPassedOut()){
-                    $this->decreaseQualityBy(1); 
-                }
-                break;
+        if($this->hasPassedOut()){
+            $this->decreaseQualityBy(1); 
         }
     }
 
     public function decreaseSellInBy($sell_in = 1)
-    {   
-        if ($this->getName() === static::SULFURAS)  return;
-        
-        $this->sell_in -= $sell_in;
-        
+    {    
+        $this->sell_in -= $sell_in;        
     }
 
     public function increaseQualityBy($quality = 1)
-    {
-        if ($this->getName() === static::SULFURAS)  return;
+    {       
 
         if ($this->quality + $quality > MAX_QUALITY) {
             $this->quality = MAX_QUALITY;
@@ -85,7 +50,6 @@ class Item {
 
     public function decreaseQualityBy($quality = 1)
     {
-        if ($this->getName() === static::SULFURAS)  return;
         
         if ($this->quality - $quality < MIN_QUALITY) {
             $this->quality = MIN_QUALITY;
@@ -93,21 +57,7 @@ class Item {
         }
         
         $this->quality -= $quality;     
-    }
-
-    public function increaseQualityForBackstagePass()
-    {
-        if ($this->getName() === static::BACKSTAGE_PASS) {
-           $this->increaseQualityBy(1);
-
-            if ($this->sell_in < 11) {
-                $this->increaseQualityBy(1);
-            }
-            if ($this->sell_in < 6) {
-                $this->increaseQualityBy(1);
-            }
-        }        
-    }
+    }    
 
     public function loseAllQuality(){
         $this->quality = 0;
