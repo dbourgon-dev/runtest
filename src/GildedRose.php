91654e07e345261679f261dd4068ce7c67d223fb
David Bourgon <dbourgon.dev@gmail.com>
2021-05-17 03:23:43 +0200

    Added increment and decrease methods for Quality and decrease for Sell in. Added boundaries check  on them.

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -2,6 +2,12 @@
 
 namespace Runroom\GildedRose;
 
+define('AGED_BRIE', 'Aged Brie');
+define('BACKSTAGE_PASS', 'Backstage passes to a TAFKAL80ETC concert');
+define('SULFURAS', 'Sulfuras, Hand of Ragnaros');
+
+use Item;
+
 class GildedRose {
 
     private $items;
@@ -12,32 +18,30 @@ class GildedRose {
 
     function update_quality() {
         foreach ($this->items as $item) {
+        
+
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                        $item->decreaseQualityBy(1);
                     }
                 }
             } else {
                 if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    $item->increaseQualityBy(1);     
                     if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                        if ($item->sell_in < 11) {                            
+                            $item->increaseQualityBy(1);  
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                        if ($item->sell_in < 6) {                            
+                            $item->increaseQualityBy(1);                            
                         }
                     }
                 }
             }
 
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
+                $item->decreaseSellInBy(1);                
             }
 
             if ($item->sell_in < 0) {
@@ -45,15 +49,15 @@ class GildedRose {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if ($item->quality > 0) {
                             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
+                                $item->decreaseQualityBy(1);                                
                             }
                         }
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->decreaseQualityBy($item->quality);
                     }
                 } else {
                     if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                        $item->increaseQualityBy(1);
                     }
                 }
             }
--- src/Item.php
+++ src/Item.php
@@ -2,6 +2,10 @@
 
 namespace Runroom\GildedRose;
 
+
+define('MAX_QUALITY', 50);
+define('MIN_QUALITY', 0);
+
 class Item {
 
     public $name;
@@ -12,6 +16,39 @@ class Item {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function decreaseSellInBy($sell_in = 1)
+    {
+        if($this->sell_in - $sell_in < 0){
+            $this->sell_in = 0;
+        }
+        $this->sell_in -= $sell_in;
+    }
+
+    public function increaseQualityBy($quality = 1)
+    {
+        if ($this->quality + $quality > MAX_QUALITY) {
+            $this->quality = MAX_QUALITY;
+            return;
+        }
+        
+        $this->quality += $quality;     
+    }
+
+    public function decreaseQualityBy($quality = 1)
+    {
+        if ($this->quality - $quality < MIN_QUALITY) {
+            $this->quality = MIN_QUALITY;
+            return;
+        }
+        
+        $this->quality -= $quality;     
     }
 
     public function __toString() {
